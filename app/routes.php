840a2fdae15b7526ea0babd31b7638b68847359c
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-04-29 13:40:35 -0500

    removed an unncecessary route

--- app/routes.php
+++ app/routes.php
@@ -36,5 +36,3 @@ Route::get('/reviews/{slug}', 'HomeController@showreview');
 Route::get('/reviews/{slug}/edit', 'HomeController@edit');
 
 Route::post('/reviews/{slug}/edit', 'HomeController@update');
-
-Route::get('/unauthorized', 'HomeController@unauthorized');
