4467c8725f1b608fb2d52ca404499f335403dc9f
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-09-06 18:38:26 -0500

    began editing the look of individual review pages

--- app/views/show.blade.php
+++ app/views/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('content')
-<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12 text-center">
+<div class="col-lg-10 col-md-8 col-sm-8 col-xs-12">
 	<img src="../{{{$reviewcover}}}">
 	<h1>"{{{$reviewtitle}}}"</h1>
 	<p class="reviewBody">{{$reviewbody}}</p>
